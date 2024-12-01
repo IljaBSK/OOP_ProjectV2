021c388bdb3d2f7a4bac4c0b276ee35ca148a5a2
Maksimsss <23327936@studentmail.ul.ie>
2024-12-01 00:26:22 +0000

    TESTING IF ITS COMMITING

--- src/FulltimeSalaryScalesReader.java
+++ src/FulltimeSalaryScalesReader.java
@@ -43,3 +43,4 @@ public class FulltimeSalaryScalesReader {
         throw new IOException("Salary not found for Job Title: " + jobTitle + " and Scale Point: " + scalePoint);
     }
 }
+//TESTING COMMITING
