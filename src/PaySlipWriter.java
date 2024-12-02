e20181c9d0ec29c5e01c0099fa679722871f894f
Maksimsss <23327936@studentmail.ul.ie>
2024-12-02 07:29:26 +0000

    forget to commit payslipwriter for the last commit

--- src/PaySlipWriter.java
+++ src/PaySlipWriter.java
@@ -1,40 +1,50 @@
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class PaySlipWriter {
     private String filePath;
 
+
+    /**
+     * Constructs a PaySlipWriter with the specified file path.
+     *
+     * @param filePath The path to the file where payslips will be written.
+     */
     public PaySlipWriter(String filePath) {
         this.filePath = filePath;
     }
 
-    public void writePayslip(String[] payslipData, LocalDate today) throws IOException {
+
+    /**
+     * Writes a payslip record to the PaySlips.csv file. If the file does not exist, a header row is added.
+     * The date field in the payslip data is formatted to MM/YYYY before writing.
+     *
+     * @param payslipData An array containing the payslip details in the following order:
+     *                    [id, name, date, jobTitle, scalePoint, grossPay, incomeTax, prsi, usc, unionFee, netPay].
+     * @throws IOException If an error occurs while writing to the file.
+     */
+    public void writePayslip(String[] payslipData) throws IOException {
         boolean fileExists = Files.exists(Paths.get(filePath));
 
-        DateTimeFormatter monthYearFormatter = DateTimeFormatter.ofPattern("MM/yyyy");
-
-        try (FileWriter writer = new FileWriter(filePath, true)) { // Append mode
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
 
             if (!fileExists) {
-                writer.write("id,name,date,jobTitle,scalePoint,grossSalary,incomeTax,prsi,usc,unionFee,netSalary\n");
+                writer.write("id,name,date,jobTitle,scalePoint,grossPay,incomeTax,prsi,usc,unionFee,netPay\n");
             }
 
-            for (int i = 5; i <= 10; i++) {
-                payslipData[i] = String.valueOf(Double.parseDouble(payslipData[i]));
-            }
 
-            payslipData[2] = today.format(monthYearFormatter);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/yyyy");
+            payslipData[2] = LocalDate.parse(payslipData[2]).format(formatter);
 
-            String line = String.join(",", payslipData);
-            writer.write(line + "\n");
-            System.out.println("Payslip written for ID: " + payslipData[0]);
-        } catch (IOException e) {
-            throw new IOException("Error writing payslip for ID: " + payslipData[0], e);
+
+            writer.write(String.join(",", payslipData) + "\n");
         }
     }
+
+
 }
